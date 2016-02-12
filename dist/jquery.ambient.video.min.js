be71df77b4f6596be9862732368547e40992cf4b
Greg LaSpina <grelas@gmail.com>
2016-02-11 18:00:33 -0700

    Move html and assets to demo folder

--- dist/jquery.ambient.video.min.js
+++ dist/jquery.ambient.video.min.js
@@ -12,8 +12,6 @@ if(typeof Object.create !== 'function') {
 (function($, window, document, undefined) {
   var AmbientVideo = {
     init: function(options, elem) {
-
-      // cache this
       var self = this;
 
       // store reference to elem
@@ -25,26 +23,19 @@ if(typeof Object.create !== 'function') {
       // extend options
       self.options = $.extend({}, $.fn.ambientVideo.defaults,  self.$elem.data(), options);
 
-      console.log('data attributes: ');
-      console.log(self.$elem.data());
-      console.log('------------------');
-      console.log(' user passed options: ');
-      console.log(options);
-      console.log('------------------');
-      console.log(' combined options ');
-      console.log(self.options);
-      console.log('------------------');
-
       if(typeof self.options.videoSrc === 'undefined') {
-        console.log('-- div does not have a `video-src` data-attribute ');
+        throw new Error('You must include a `video-src` data-attribute');
       }
 
       // kick it off
       self.build();
     },
 
+    /**
+     * [build description]
+     * @return {[type]} [description]
+     */
     build: function(){
-      console.log('BUILD VIDEO');
       var self = this;
 
       //check to see if browser can support html5 video
@@ -52,7 +43,6 @@ if(typeof Object.create !== 'function') {
       if(self.check().supports_video && self.options.videoSrc !== null && !self.check().is_iOS && !self.check().is_Android) {
 
         self.$elem.waypoint(function(){
-          console.log(' -- insert video');
           self.insertVideo();
         }, {
           offset: self.options.videoOffset,
@@ -62,9 +52,12 @@ if(typeof Object.create !== 'function') {
       } else {
         self.fallback();
       }
-
     },
 
+    /**
+     * [check description]
+     * @return {[type]} [description]
+     */
     check: function(){
       return {
         supports_video: !!document.createElement('video').canPlayType,
@@ -73,13 +66,18 @@ if(typeof Object.create !== 'function') {
       };
     },
 
+    /**
+     * [insertVideo description]
+     * @return {[type]} [description]
+     */
     insertVideo: function(){
       var self = this;
 
-      self.video_src_html = '<video preload="metadata" muted>' +
-                            '<source src="'+ self.options.videoSrc + '.webm" type="video/webm; codecs=vp8,vorbis">' +
-                            '<source src="'+ self.options.videoSrc + '.mp4" type="video/mp4; codecs=avc1.42E01E,mp4a.40.2">' +
-                            '</video>';
+      self.video_src_html = '' +
+        '<video preload="metadata" muted>' +
+          '<source src="'+ self.options.videoSrc + '.webm" type="video/webm; codecs=vp8,vorbis">' +
+          '<source src="'+ self.options.videoSrc + '.mp4" type="video/mp4; codecs=avc1.42E01E,mp4a.40.2">' +
+        '</video>';
 
       // html5 video
      // self.video_html = '<video preload muted>' + self.video_src_html + '</video>';
@@ -94,17 +92,6 @@ if(typeof Object.create !== 'function') {
       // store jquery version of the video node
       self.$video_dom = $(self.video_dom);
 
-      // Disable visibility, while loading
-      //self.$video_dom.css("visibility", "hidden");
-
-      // check to see if video should loop
-      // if(self.options.videoLoop) {
-      //   console.log(' -- video should loop');
-      //   self.video_dom.loop = true;
-      // } else {
-      //   console.log(' -- video should NOT loop');
-      // }
-
       self.$video_dom.prop({
         loop: self.options.videoLoop
       });
@@ -112,14 +99,15 @@ if(typeof Object.create !== 'function') {
       self.baseClass();
       self.load();
 
-      // Wait until readyState is 4 (if you don't specify a poster, in IE before the video loads there will be a black box)
-      // Enough data is available—and the download rate is high enough—that the media can be played through to the end without interruption
+      // Wait until readyState is 4 (if you don't specify a poster,
+      // in IE before the video loads there will be a black box)
+      // Enough data is available—and the download rate is high
+      // enough—that the media can be played through to the end
+      // without interruption
+      //
       // https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
       function checkReadyState() {
-        console.log(self.video_dom.readyState);
-
         if (self.video_dom.readyState === 4) {
-          console.log('readystate === 4');
           self.$video_dom.addClass('ambient-video-loaded');
           self.play();
         } else {
@@ -129,28 +117,8 @@ if(typeof Object.create !== 'function') {
 
       checkReadyState();
 
-      // self.$video_dom.on('loadeddata', function(){
-      //   console.log('video loadeddata loaded');
-      //   self.$video_dom.addClass('ambient-video-loaded');
-      // });
-
-      // self.$video_dom.on('loadedmetadata', function(){
-      //   console.log('video loadedmetadata loaded');
-
-      // });
-
-      // self.$video_dom.on('canplaythrough', function(){
-      //   console.log('video canplaythrough loaded');
-      //   self.$video_dom.addClass('ambient-video-loaded');
-      //   self.play();
-      // });
-
-
       self.$video_dom.on('ended', function(){
-        console.log('video ended');
-
         self.pause();
-        console.log(self.video_dom.paused);
 
         if(typeof self.options.onComplete === 'function') {
           self.options.onComplete.apply(self.elem, arguments);
@@ -159,6 +127,10 @@ if(typeof Object.create !== 'function') {
 
     },
 
+    /**
+     * [baseClass description]
+     * @return {[type]} [description]
+     */
     baseClass: function(){
       var self = this;
 
@@ -169,33 +141,47 @@ if(typeof Object.create !== 'function') {
       }
     },
 
+    /**
+     * [load description]
+     * @return {[type]} [description]
+     */
     load: function(){
-      console.log(' -- load video');
       var self = this;
       self.video_dom.load();
     },
 
+    /**
+     * [play description]
+     * @return {[type]} [description]
+     */
     play: function(){
       var self = this;
 
       if(self.video_dom.paused) {
-        console.log(' -- play video');
         self.video_dom.play();
       }
     },
 
+    /**
+     * [pause description]
+     * @return {[type]} [description]
+     */
     pause: function(){
       var self = this;
 
       if(!self.video_dom.paused) {
-        console.log(' -- pause video');
         self.video_dom.pause();
         self.is_video_playing = false;
       }
     },
 
+    /**
+     * [toggle description]
+     * @return {[type]} [description]
+     */
     toggle: function(){
       var self = this;
+
       if(self.video_dom.paused) {
         self.play();
       } else {
@@ -203,35 +189,35 @@ if(typeof Object.create !== 'function') {
       }
     },
 
+    /**
+     * [replay description]
+     * @return {[type]} [description]
+     */
     replay: function(){
-      console.log(' -- replay video');
       var self = this;
       self.video_dom.currentTime = 0;
       self.video_dom.play();
     },
 
+    /**
+     * [fallback description]
+     * @return {[type]} [description]
+     */
     fallback: function(){
-      console.log('show fallback');
+      var self = this;
+      var fallback_img;
 
-      var self = this,
-          fallback_img;
-
-      if(self.options.videoFallback !== null) {
+      if (self.options.videoFallback !== null) {
 
         fallback_img = $('<img />')
           .attr('src', self.options.videoFallback )
           .load(function() {
-              if(!this.complete || typeof this.naturalWidth === 'undefined' || this.naturalWidth === 0) {
-                  console.log('broken image!');
-              } else {
-                $(this).remove();
-                self.$elem.css('background-image', 'url(' + self.options.videoFallback + ')');
-              }
+            $(this).remove();
+            self.$elem.css('background-image', 'url(' + self.options.videoFallback + ')');
           });
       }
     }
   };
-
 
   $.fn.ambientVideo = function(options) {
 
